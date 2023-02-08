c14fa1c1b80b4c70e6e76b6838ae2ea472422a9b
Giordano <giordano.mendes94@gmail.com>
2023-02-07 21:07:18 -0300

    feat: app  Product routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import productRoute from './Routes/productRouter';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productRoute);
 
 export default app;
