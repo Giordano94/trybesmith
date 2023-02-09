6b5ebccdbd8d21be7e07ec31ded8011d07764ad8
Giordano <giordano.mendes94@gmail.com>
2023-02-08 21:46:31 -0300

    feat: app  User Route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 
 import productRoute from './Routes/productRouter';
+import userRoute from './Routes/UserRouter';
+import orderRoute from './Routes/OrderRouter';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productRoute);
+app.use('/users', userRoute);
+app.use('/orders', orderRoute);
 
 export default app;
